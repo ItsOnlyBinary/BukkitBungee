fa8c1dd42d5a0cd3ce9fd4f365716d7dec53f47c
NucWin <nucwin@gmail.com>
2014-08-26 13:43:33 +0100

    Added permissions for commands /server and /glist;

--- src/main/java/me/pvmac2194/bukkitbungee/CommandHandler.java
+++ src/main/java/me/pvmac2194/bukkitbungee/CommandHandler.java
@@ -34,59 +34,73 @@ public class CommandHandler implements Listener, CommandExecutor
       {
         if (commandLabel.equalsIgnoreCase("server"))
           {
-            plugin.senderPlayer = (Player) sender;
-            if (args.length == 0)
+            if (sender.hasPermission("bukkitbungee.server"))
               {
-                plugin.gettingServerList = false;
-                plugin.getCurrentServer();
-                plugin.getAllServers();
-
-                plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable()
+                plugin.senderPlayer = (Player) sender;
+                if (args.length == 0)
                   {
-                    public void run()
+                    plugin.gettingServerList = false;
+                    plugin.getCurrentServer();
+                    plugin.getAllServers();
+
+                    plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable()
                       {
-                        plugin.senderPlayer.sendMessage(colorString(plugin.getConfig().getString("currentserver")).replace("{CURRENTSERVER}",
-                            plugin.currentServer));
-                        plugin.senderPlayer.sendMessage(colorString(plugin.getConfig().getString("serverlist")).replace("{SERVERLIST}",
-                            plugin.serverListString));
-                      }
-                  }, 2L);
+                        public void run()
+                          {
+                            plugin.senderPlayer.sendMessage(colorString(plugin.getConfig().getString("currentserver")).replace("{CURRENTSERVER}",
+                                plugin.currentServer));
+                            plugin.senderPlayer.sendMessage(colorString(plugin.getConfig().getString("serverlist")).replace("{SERVERLIST}",
+                                plugin.serverListString));
+                          }
+                      }, 2L);
 
+                  }
+                else if (args.length == 1)
+                  {
+                    ByteArrayOutputStream b = new ByteArrayOutputStream();
+                    DataOutputStream out = new DataOutputStream(b);
+                    try
+                      {
+                        // Gets current server
+                        out.writeUTF("Connect");
+                        out.writeUTF(args[0]);
+                      }
+                    catch (Exception e)
+                      {
+                        // ignore
+                      }
+                    plugin.senderPlayer.sendPluginMessage(plugin, "BungeeCord", b.toByteArray());
+                  }
               }
-            else if (args.length == 1)
+            else
               {
-                ByteArrayOutputStream b = new ByteArrayOutputStream();
-                DataOutputStream out = new DataOutputStream(b);
-                try
-                  {
-                    // Gets current server
-                    out.writeUTF("Connect");
-                    out.writeUTF(args[0]);
-                  }
-                catch (Exception e)
-                  {
-                    // ignore
-                  }
-                plugin.senderPlayer.sendPluginMessage(plugin, "BungeeCord", b.toByteArray());
+                  sender.sendMessage(colorString(plugin.getConfig().getString("noperms")));
               }
           }
         else if (commandLabel.equalsIgnoreCase("glist"))
           {
-            plugin.senderPlayer = (Player) sender;
-            plugin.gettingServerList = true;
-            if (!(plugin.getConfig().getString("beforeglist").equals("none")))
+            if (sender.hasPermission("bukkitbungee.server"))
               {
-                sender.sendMessage(colorString(plugin.getConfig().getString("beforeglist")));
-              }
-            plugin.getAllServers();
-            plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable()
-              {
-                public void run()
+                plugin.senderPlayer = (Player) sender;
+                plugin.gettingServerList = true;
+                if (!(plugin.getConfig().getString("beforeglist").equals("none")))
                   {
-                    plugin.getAllPlayers();
-                    plugin.getAllPlayersInCount();
+                    sender.sendMessage(colorString(plugin.getConfig().getString("beforeglist")));
                   }
-              }, 2L);
+                plugin.getAllServers();
+                plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable()
+                  {
+                    public void run()
+                      {
+                        plugin.getAllPlayers();
+                        plugin.getAllPlayersInCount();
+                      }
+                  }, 2L);
+              }
+            else
+              {
+                  sender.sendMessage(colorString(plugin.getConfig().getString("noperms")));
+              }
           }
         else if (cmd.getName().equalsIgnoreCase("bbreload"))
           {
@@ -94,6 +108,10 @@ public class CommandHandler implements Listener, CommandExecutor
               {
                 plugin.reloadConfig();
                 sender.sendMessage(ChatColor.GREEN + "The configuration has been reloaded.");
+              }
+            else
+              {
+                  sender.sendMessage(colorString(plugin.getConfig().getString("noperms")));
               }
           }
 
